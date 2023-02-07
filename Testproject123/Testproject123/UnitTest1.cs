8ea37ec44ff587b637d5a5efdf37f214f85e4570
Waz Haider <waz.haider@advantest.com>
2023-02-07 14:53:04 -0800

    created a unit test

--- Testproject123/Testproject123/UnitTest1.cs
+++ Testproject123/Testproject123/UnitTest1.cs
@@ -5,6 +5,12 @@ namespace Testproject123
         [Fact]
         public void Test1()
         {
+            int i = 10;
+            int j = 33;
+            int result = i + j;
+            int expected = 43;
+                Assert.Equal(expected, result);    
+ 
 
         }
     }
